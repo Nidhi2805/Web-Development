40c26f049fa1236efa115c37aebfb2a433621867
Nidhi2805 <nidhianand.karva2023@vitstudent.ac.in>
2025-04-02 07:40:10 +0530

    Landing Page

--- web-project/src/components/Explore.js
+++ web-project/src/components/Explore.js
@@ -3,21 +3,18 @@ import React from 'react';
 const Explore = () => {
     return (
         <div style={{ display: 'flex', alignItems: 'center', height: '100vh', width: '80%', margin: '-6rem 0 ' }}>
-            {/* Left-side Image */}
             <img 
                 src="/explore.png" 
                 alt="Explore Image" 
                 style={{ width: '36%', height: 'auto', objectFit: 'cover' }}
             />
-            
-            {/* Right-side Content */}
+
             <div style={{ flex: 1, paddingLeft: '3rem', display: 'flex', flexDirection: 'column', marginTop: "-3rem" }}>
                 <h1 style={{ fontSize: '2.5rem', fontWeight: 'bold' }}>Artfully Crafted</h1>
                 <p style={{ fontSize: '1.25rem', color: '#555', marginTop: "-1rem" }}>
                     Explore our diverse menu, featuring a wide array of mouthwatering dishes
                 </p>
 
-                {/* Boxes Section */}
                 <div style={{ display: 'flex', justifyContent: 'flex-start', gap: '1.5rem', marginTop: '1.5rem' }}>
                     {[
                         { img: "/apple.png", text: "Explore Our Menu", desc: "Discover a world of culinary delights, from classic favorites to innovative " },
